8f5e91c5009dffb585b3e71fecaf79adccdab878
Peter Tonner <peter.tonner@proton.me>
2023-11-24 12:07:03 -0500

    refactor: move everything into a single global context

--- src/ptonner/mini_maniac.clj
+++ src/ptonner/mini_maniac.clj
@@ -2,19 +2,21 @@
   (:require [lambda-toolshed.papillon :as pap]
             [flatland.ordered.map :refer [ordered-map]]))
 
-(def ^:dynamic param-store nil)
-(def ^:dynamic maniac-stack nil)
-(def ^:dynamic maniac-ctx nil)
+(def ^:dynamic context (atom {}))
+
+(defn- context-init []
+  {::interceptors (ordered-map)})
 
 ;; Interceptors
 
 (defn trace "Trace interceptor to record primitive sites in order"
-  [& {:keys [name] :or {name ::default}}]
-  {:leave (fn [{::keys [message] :as ctx}]
-            (let [message-name (::name message)]
-              (update-in ctx [::trace name]
-                         (fnil assoc (ordered-map))
-                         message-name message)))})
+  ([] (trace ::default))
+  ([name]
+   {:leave (fn [{::keys [message] :as ctx}]
+             (let [message-name (::name message)]
+               (update-in ctx [::trace name]
+                          (fnil assoc (ordered-map))
+                          message-name message)))}))
 
 (defn get-trace "Get the output of a trace"
   [ctx name]
@@ -24,7 +26,7 @@
   [prev]
   {:enter (fn [{::keys [message] :as ctx}]
             (let [guide (get-in ctx [::trace prev])
-                  name (:name message)]
+                  name (::name message)]
               (assoc-in ctx [::message ::value]
                         (get-in guide [name ::value]))))})
 
@@ -34,67 +36,50 @@
   {:enter (fn [{::keys [message] :as ctx}]
             (assoc-in ctx [::message ::value]
                       (or (::value message)
-                          ((get message ::fn identity)
+                          (apply
+                           (get message ::fn identity)
                            (::args message)))))})
 
+(defmacro with-context [& body]
+  `(binding [context (atom (context-init))]
+     ~@body))
+
+(defmacro with-interceptor [ix & body]
+  (let [name (keyword (gensym name))
+        ignore (gensym "ignore")
+        res (gensym "res")]
+    `(let [~ignore (swap! context assoc-in
+                          [::interceptors ~name] ~ix)
+           ~res (do ~@body)
+           ~ignore (swap! context dissoc ::interceptors ~name)]
+       ~res)))
+
 ;; API
-(defmacro with-stack "Wrap execution with stack"
-  [stack & body]
-  `(binding [maniac-stack ~stack]
-     ~@body))
 
-(defmacro with-ctx [& body]
-  `(binding [maniac-ctx (atom {})]
-     ~@body))
-
-(defn process-message "Process a message using provided interceptors"
-  [ctx ixs message]
-  (pap/execute ixs (assoc ctx ::message message)))
-
+(defn- process-message "Process a message using provided interceptors"
+  [ctx message]
+  (pap/execute (-> ctx ::interceptors vals)
+               (assoc ctx ::message message)))
 (comment
-  (process-message {} [(trace) (message-value)] {::fn identity ::args 0 ::name :test}))
+  (::message (process-message {::interceptors
+                               (ordered-map :trace (trace)
+                                            :mval (message-value) )}
+                              {::fn identity ::args [0] ::name :test})))
 
 (defn sample [name fun & {:as args}]
   (let [message {::type ::sample
                  ::name name
                  ::fn fun
                  ::args args}
-        ctx (swap! maniac-ctx process-message maniac-stack message)]
+        ctx (swap! context process-message message)]
     (get-in ctx [::message ::value])))
 
-(with-ctx
-  (with-stack [(message-value)]
-    (sample :test)))
+(comment (with-context
+           (with-interceptor (trace :guide)
+             (with-interceptor (message-value)
+               (sample :a identity [0])))
+           (with-interceptor (replay :guide)
+             (with-interceptor (message-value)
+               (sample :a identity [1])))))
 
-(comment
-  (defn- process-queue [ctx]
-    (if-let [next (some-> ctx ::queue peek)]
-      (let [enter (:enter next)
-            ctx (if enter (enter ctx) ctx)]
-        (recur
-         (-> ctx (update ::queue pop)
-             (update ::stack (fnil conj []) next))))
-      ctx))
 
-  (defn- process-stack [ctx]
-    (if-let [next (some-> ctx ::stack peek)]
-      (let [exit (:leave next)
-            ctx (if exit (exit ctx) ctx)]
-        (recur
-         (update ctx ::stack pop)))
-      ctx))
-
-  (defn process
-    "Basic interceptor processing"
-    [ctx xtors]
-    (-> ctx (assoc ::queue (apply list xtors)) process-queue process-stack (dissoc ::queue ::stack))))
-
-(comment)
-(pap/execute [{:enter #(update % :a (fnil inc 0)) :leave (fn [c] (-> c (assoc :b (:a c))))}
-              {:enter #(update % :a (fnil * 0) 7) :leave #(dissoc % :c)}
-              {:leave #(update % :a (fnil / 0) 5)}])
-
-(comment
-  (process {} [{:enter #(update % :a (fnil inc 0)) :leave (fn [c] (-> c (assoc :b (:a c))))}
-               {:enter #(update % :a (fnil * 0) 7) :leave #(dissoc % :c)}
-               {:leave #(update % :a (fnil / 0) 5) :name :c}]))
